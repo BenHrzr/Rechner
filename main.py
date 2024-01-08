0260ee27616e8fc06a8d771ce472d080269289a5
Ru1ned <145772142+ifaqurmoma69@users.noreply.github.com>
2024-01-08 17:48:30 +0100

    NICHT VOLLSTÄNDIG

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+calculation = ""
+
+def add_to_calculation(symbol):
+    pass
+
+def evaluate_calculation():
+    pass
+
+def clear_field():
+    pass
+
+root = tk.Tk()
+root.geometry("400x375")
+
+text_result = tk.Text(root, height=2.5, width=16, font=("Arial", 24))
+text_result = Grid(columspan=5)
+
+root.mainloop()
