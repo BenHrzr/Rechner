0927f966c5056ca04536f3d6a99a48af0b848cb7
Ben | Ru1ned <145772142+BenHrzr@users.noreply.github.com>
2024-04-14 15:31:15 +0200

    Update main(IF).py
    You can NOW only type number in "zahl1" and "zahl2". Furthermore the usage of a diffrent type of calculation is now prohibited too.

--- main(IF).py
+++ main(IF).py
@@ -1,32 +1,33 @@
 import time
 i=0
-ergebnis = 0
 
-zahl1 = int(input('Was ist die erste Zahl?'))
-rechenart = str(input('Welche Rechenart?'))
-zahl2 = int(input('Was ist die zweite Zahl?'))
-#zahl3 = int(input('Was ist die dritte Zahl'))
 
 while i<= 10:
+    try: 
 
-    if rechenart == '+':
-        ergebnis = zahl1+zahl2
-            #{zahl1} {rechenart} {zahl1) == {zahl1+zahl2}  <--- (ALTERNATIVE)
-    elif rechenart == '-':
-        ergebnis = zahl1-zahl2
-    elif (rechenart == ':' or rechenart == '/'):
-        ergebnis = zahl1/zahl2
-    elif (rechenart == 'x' or rechenart =='*'):
-        ergebnis = zahl1*zahl2
-    else print('REchenfehler!')
+        ergebnis = 'Fehlerhafte Eingabe'
 
-    print(f'{ergebnis}')
+        zahl1 = int(input("Was ist die erste Zahl?"))
+        rechenart = str(input('Welche Rechenart?'))
+        zahl2 = int(input("Was ist die zweite Zahl?"))
+
+        
+
+        if rechenart == '+':
+            ergebnis = zahl1+zahl2
+                #{zahl1} {rechenart} {zahl1) == {zahl1+zahl2}  <--- (ALTERNATIVE)
+        elif rechenart == '-':
+            ergebnis = zahl1-zahl2
+        elif (rechenart == ':' or rechenart == '/'):
+            ergebnis = zahl1/zahl2
+        elif (rechenart == 'x' or rechenart =='*'):
+            ergebnis = zahl1*zahl2
+            
+        print(ergebnis)
+        
+    except ValueError: 
+        print('Fehlerhafte Eingabe. Bitte stellen Sie sicher, dass Sie nur Zahlen eingeben.')
+    
     time.sleep(1.3)
 
 
-
-
-# To do:
-#nur zahlen angeben können
-#neue rechnung?
-#3, oder mehr zahlen
